15fa40baae473f238da4f848206829d4251ddf66
Flo <53355483+Flo4604@users.noreply.github.com>
2024-06-28 17:33:30 +0200

    fix: Bump version

--- GrindSaleItemsIntoGems/index.js
+++ GrindSaleItemsIntoGems/index.js
@@ -2,7 +2,7 @@
 // @name         Grind Sale Items Into Gems
 // @icon         https://store.steampowered.com/favicon.ico
 // @namespace    steam
-// @version      1.1.5
+// @version      1.2.0
 // @description  Choose how many and what sale items you want to grind into gems in few clicks
 // @author       Flo4604, oroginal by Lutymane
 // @license MIT
